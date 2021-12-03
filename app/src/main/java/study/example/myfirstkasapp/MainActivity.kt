b6f193c3a21d2150beffbf085b9418ff5b510ffa
knovle <ov.penkrat@gmail.com>
2021-12-03 05:27:11 +0400

    first commit

--- app/src/main/java/study/example/myfirstkasapp/MainActivity.kt
+++ app/src/main/java/study/example/myfirstkasapp/MainActivity.kt
@@ -2,10 +2,54 @@ package study.example.myfirstkasapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Вот приходит замысел рисунка.")
     }
+
+    override fun onStart() {
+        super.onStart( )
+
+        Log.d(TAG, "Поединок сердца и рассудка.")
+    }
+
+    override fun onResume() {
+        super.onResume( )
+
+        Log.d(TAG, "Иногда рассудок побеждает:")
+    }
+
+    override fun onPause() {
+        super.onPause( )
+
+        Log.d(TAG, "он довольно трезво рассуждает,")
+    }
+
+    override fun onStop() {
+        super.onStop( )
+
+        Log.d(TAG, "здравые высказывает мысли -")
+    }
+
+    override fun onRestart() {
+        super.onRestart( )
+
+        Log.d(TAG, "ну, и побеждает в этом смысле...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy( )
+
+        Log.d(TAG, "Юрий Левитанский")
+    }
+
 }
